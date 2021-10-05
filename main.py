70667f7254832d4858daf305281efec4bc67b05d
Sophia <49248015+sparrett@users.noreply.github.com>
2021-10-05 09:55:16 -0700

    almost full implementation

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ from csv import reader
 
 def main(): 
     data = unpack_data_set("SHORT_receipts_&_goods.csv")
-    support_counts = find_support_counts(data)
-    remove_less_than_support_counts(support_counts, 0)
-    make_pairs(support_counts, data)
+    goods = unpack_goods("SHORT_goods.csv")
+    good_ids = goods.keys()
+
+    full_implementation(good_ids, data, 2)
+    
 
 
 def unpack_data_set(filename):
@@ -31,89 +33,171 @@ def unpack_data_set(filename):
         # Iterate over each row in the csv using reader object
         for row in csv_reader:
             # row variable is a list that represents a row in csv
-            original_data.append(row)
+            int_ = []
+            for item in row: 
+                int_.append(int(item))
+            
+            original_data.append(int_)
+
+    print(original_data)
     return original_data
 
+def unpack_goods(filename):
+    goods = {}
+    with open(filename, 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Iterate over each row in the csv using reader object
+        i = 0 
+        for row in csv_reader:
+            if i > 1: 
+            # row variable is a list that represents a row in csv
+                goods[int(row[0])] = row[1]
+            i+=1
 
-def find_support_counts(data): 
-    support_counts = {}
-    print("data", data)
-
-#   item id    number of times it shows up
-    
-    for item_list in data:   
-        # ignore the first one because its the receipt 
-        for i in range(1, len(item_list)):
-            if item_list[i] in support_counts:
-                support_counts[item_list[i]] += 1
-            else: 
-                support_counts[item_list[i]] = 1
-
-    print("support count dict", support_counts)
-    return support_counts
+    return goods 
 
 
 
-def remove_less_than_support_counts(data, min_support): 
-    for key, value in list(data.items()):
-        if value <= min_support: 
-            del data[key]
-
-    print(data)
-
-
-
-def make_pairs(support_counts, data): 
-
-    # data 
-    # list of lists of all data 
-    #  [[ 1, 4, 5, 6, 10] 
-    #  [2, 1, 12, 19, 46, 47]
-    #  [3, 16, 32]
-    #  [4, 18, 24, 35]] 
-
-    list_of_item_ids = support_counts.keys() # 1 2 3 4 
-    print("item_ids ", list(list_of_item_ids))
-    list_of_pairs = list_of_pairs_helper(list(list_of_item_ids), 2)#  (1,2)  (1,3) (3,4)
-    print("list of pairs ", list_of_pairs)
-    dict_of_pairs = {}
-
-# JOSH LOOK AT MY TEXT I THINK IT WORKS NOW 
-    # dict of pairs:
-    #    pair    support count  
-    #  { (1,2) :  4 }
-
-    for item_lists in data: 
-        for pair_set in list_of_pairs: #1,4
-            if check_if_tuple_in_list(pair_set, item_lists):  #1, 4, 5, 6, 10
-                if pair_set in dict_of_pairs: 
-                    dict_of_pairs[pair_set] += 1
-                else: 
-                    dict_of_pairs[pair_set] = 1
-
-    print(dict_of_pairs)
-
-
-
-def check_if_tuple_in_list(tuple_, list_): 
-    # returns true if tuple is in list (1-end)
-    list_minus_first = list_[1:]
-    print("\n")
-    print("list_minus_first ", list_minus_first)
-    print("tuple check", (tuple_[0] in list_minus_first) and (tuple_[1] in list_minus_first))
-    print("tuple & list ", tuple_, list_minus_first )
-    return (tuple_[0] in list_minus_first) and (tuple_[1] in list_minus_first)
-
-
-       
 
 def list_of_pairs_helper(stuff, length): 
     pair_ret = []
     for i in range(0, len(stuff)+1):
         for subset in itertools.combinations(stuff, i):
-            if len(subset) == length:
-                pair_ret.append(subset)
+            if len(subset) == length or length == 0:
+                pair_ret.append(list(subset))
     return pair_ret
+
+
+def find_support_count(data, t):  # t = tuple to find support count of 
+    support_count = 0
+  
+    for receipt_list in data:  
+        receipt_set = set(receipt_list[1:])
+        # print(set(receipt_list[1:]))
+        # print(set(t))
+        # print(set(t).issubset(receipt_set))
+        if set(t).issubset(receipt_set): 
+            support_count += 1
+
+    return support_count
+    
+
+def delete_instance_from_tree(list_of_sets, delete): 
+    overall_list = list_of_sets.copy()
+    for i in list_of_sets:
+        if set(delete).issubset(i):
+           overall_list.remove(i)
+    return overall_list
+
+
+
+def full_implementation(good_ids, data, minSup): 
+
+    list_of_sets = list_of_pairs_helper(good_ids, 0) # edit this so its all the sizes 
+
+    for set_ in list_of_sets:   # organized in order of size smallest to large (2,3)  (3,4,2) 
+        x = find_support_count(data, set_)
+        print("\nfor ", set_)
+        print(x)
+        
+        if x < minSup: 
+            list_of_sets = delete_instance_from_tree(list_of_sets, set_)
+
+        print(list_of_sets)
+
+    print("final ", list_of_sets)
+
+
+    # delete possible set values 
+
+
+
+
+
+
+
+
+
+# def remove_less_than_support_counts(data, min_support): 
+#     for key, value in list(data.items()):
+#         if value <= min_support: 
+#             del data[key]
+
+#     print(data)
+
+
+
+# def make_pairs(support_counts, data): 
+
+#     # data 
+#     # list of lists of all data 
+#     #  [[ 1, 4, 5, 6, 10] 
+#     #  [2, 1, 12, 19, 46, 47]
+#     #  [3, 16, 32]
+#     #  [4, 18, 24, 35]] 
+
+#     list_of_item_ids = support_counts.keys() # 1 2 3 4 
+#     print("item_ids ", list(list_of_item_ids))
+#     list_of_pairs = list_of_pairs_helper(list(list_of_item_ids), 2)#  (1,2)  (1,3) (3,4)
+#     print("list of pairs ", list_of_pairs)
+#     dict_of_pairs = {}
+
+# # JOSH LOOK AT MY TEXT I THINK IT WORKS NOW 
+#     # dict of pairs:
+#     #    pair    support count  
+#     #  { (1,2) :  4 }
+
+#     for item_lists in data: 
+#         for pair_set in list_of_pairs: #1,4
+#             if check_if_tuple_in_list(pair_set, item_lists):  #1, 4, 5, 6, 10
+#                 if pair_set in dict_of_pairs: 
+#                     dict_of_pairs[pair_set] += 1
+#                 else: 
+#                     dict_of_pairs[pair_set] = 1
+
+#     print(dict_of_pairs)
+
+
+
+
+
+
+
+# def full_implementation(): 
+
+#     list_of_sets = list_of_pairs_helper() # edit this so its all the sizes 
+
+#     for set in list_of_sets:   # organized in order of size smallest to large (2,3)  (3,4,2) 
+#         if support < minSup 
+#             helper()        # (1,2)  sup = 3       
+
+
+
+#   support counts    A   B     C    D    E  
+                    # deletions rest of tree 
+
+#  support counts     AB    BC     DE    
+
+
+
+
+
+
+
+# def check_if_tuple_in_list(tuple_, list_): 
+#     # returns true if tuple is in list (1-end)
+#     list_minus_first = list_[1:]
+#     print("\n")
+#     print("list_minus_first ", list_minus_first)
+#     print("tuple check", (tuple_[0] in list_minus_first) and (tuple_[1] in list_minus_first))
+#     print("tuple & list ", tuple_, list_minus_first )
+#     return (tuple_[0] in list_minus_first) and (tuple_[1] in list_minus_first)
+
+
+       
+
+
                 
             
 #[ 1, 4, 5, 6, 10
@@ -138,27 +222,27 @@ def list_of_pairs_helper(stuff, length):
 
 
 # step one : Frequent items 
-def apriori(T, I, min_support): 
-    """
-    def: finds the frequent items sets 
-    PARAM: 
-        min_support: (float) between 0.0-0.999
-        T: market basket data set 
-        I: List of items 
+# def apriori(T, I, min_support): 
+#     """
+#     def: finds the frequent items sets 
+#     PARAM: 
+#         min_support: (float) between 0.0-0.999
+#         T: market basket data set 
+#         I: List of items 
 
-    """
-    pass 
+#     """
+#     pass 
 
 
-def canidate_gen(): 
-    """
-    helper function frequent items 
-    """
-    pass 
+# def canidate_gen(): 
+#     """
+#     helper function frequent items 
+#     """
+#     pass 
 
-# step two 
-def association_rules(): 
-    pass 
+# # step two 
+# def association_rules(): 
+#     pass 
 
 
 if __name__ == "__main__": 
